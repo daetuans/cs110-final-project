e83bdd7318980a5abdb2ec42436f99f391f1b21e
JanaT73 <TurqDmn73@yahoo.com>
2016-12-13 13:50:51 -0500

    Add files via upload

--- classStatus.py
+++ classStatus.py
@@ -1,1 +1,21 @@
+from tkinter import *
+from classDialogue import *
+
 class Status:
+  def __init__(self):
+    self.__colorBlank = 'white'
+    self.__colorFill = 'gray'
+
+  def getDefaultColor(self):
+    return self.__colorBlank
+
+  def getFillColor(self):
+    return self.__colorFill
+
+##def main():
+##  s = Status()
+##  print(s.getDefaultColor())
+##
+##main()
+    
+        
